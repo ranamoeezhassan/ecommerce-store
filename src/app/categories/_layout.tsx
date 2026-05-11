d4144b6e5c9d422c8469373aa10f4dfc9190af45
ranamoeezhassan <rmhass26@colby.edu>
2026-05-11 17:54:19 +0400

    Setup layouts for expo router

---
+++ src/app/categories/_layout.tsx
@@ -0,0 +1,20 @@
+import { Stack } from "expo-router";
+import { Touchable, TouchableOpacity } from "react-native";
+import { Ionicons } from '@expo/vector-icons';
+
+
+export default function CategoryLayout() {
+  return (
+    <Stack>
+      <Stack.Screen
+        name="[slug]"
+        options={({navigation}) => ({
+            headerShown: false,
+            headerLeft: () => <TouchableOpacity onPress={() => navigation.goBack()}>
+              <Ionicons name="arrow-back" size={24} color="black"/>
+            </TouchableOpacity>
+          })}
+      />
+    </Stack>
+  );
+}
